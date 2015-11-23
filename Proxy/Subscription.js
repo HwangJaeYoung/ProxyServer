fdc63803ce81032450485a886a4a8209c5ce166f
HwangJaeYoung <forest62590@naver.com>
2015-11-23 16:41:46 +0900

    Add error catch code to Subscription.js

--- Proxy/Subscription.js
+++ Proxy/Subscription.js
@@ -35,18 +35,26 @@ var updateFunction = function(response, entityName, attributeName, type, value, 
             "content": value[subscriptionCount]
         }
     }, function (error, contentInstanceResponse, body) {
-        if (contentInstanceResponse.statusCode == 201) { // 정상적으로 등록이 다 되었을 때
+        if(typeof(contentInstanceResponse) !== 'undefined') {
+            if (contentInstanceResponse.statusCode == 201) { // 정상적으로 등록이 다 되었을 때
 
-            if (subscriptionCount < attributeName.length - 1) {
-                subscriptionCount++;
-                updateCallback(response, entityName, attributeName, type, value, startTime, subscriptionCount, updateFunction);
+                if (subscriptionCount < attributeName.length - 1) {
+                    subscriptionCount++;
+                    updateCallback(response, entityName, attributeName, type, value, startTime, subscriptionCount, updateFunction);
+                } else {
+                    subscriptionCount = 0; // 모두 다 업데이트 하였으므로 초기화 한다.
+                }
             } else {
-                subscriptionCount = 0; // 모두 다 업데이트 하였으므로 초기화 한다.
+                // 주로 409 Conflict에러가 발생한다.
+                console.log(contentInstanceResponse.statusCode);
+                process.exit(1); // 에러가 발생했을 경우 서버를 종료시킨다.
             }
         } else {
-            // 주로 409 Conflict에러가 발생한다.
-            console.log(contentInstanceResponse.statusCode);
-            process.exit(1); // 에러가 발생했을 경우 서버를 종료시킨다.
+            if(error != null) {
+                console.log('****************** error status **************** :' + error);
+                console.log('****************** error code ****************** : ' + error.code);
+                process.exit(1); // 에러가 발생했을 경우 서버를 종료시킨다.
+            }
         }
     });
 };
